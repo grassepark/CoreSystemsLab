2c3349a2fb288dd9b475736fe45b2fa674fb6fe9
Your Name <122640719+grassepark@users.noreply.github.com>
2023-11-04 18:40:48 -0400

    data

--- week-08/CityAsSystems/js/site.js
+++ week-08/CityAsSystems/js/site.js
@@ -14,8 +14,44 @@ courtyard.addEventListener('mouseout', () => {
 
 ampitheatre.addEventListener('mouseover', () => {
     popup2.style.opacity = '1';
+    console.log("fart")
 });
 
 ampitheatre.addEventListener('mouseout', () => {
     popup2.style.opacity = '0';
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    function loadJSONAndUpdateSVG(jsonURL) {
+        //change the json based off of button press
+        fetch(jsonURL)
+            .then(response => response.json())
+            .then(data => {
+                for (const circleId in data) {
+                    const circleData = data[circleId];
+                    const circle = document.getElementById(circleId);
+        
+                    if (circle) {
+                        circle.setAttribute('stroke-width', circleData.strokeWidth);
+                    }
+                }
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }
+
+    // morning
+    document.getElementById("button1").addEventListener("click", function () {
+        loadJSONAndUpdateSVG("morning.json"); 
+    });
+
+    // noon
+    document.getElementById("button2").addEventListener("click", function () {
+        loadJSONAndUpdateSVG("noon.json"); 
+    });
+
+    // evening
+    document.getElementById("button3").addEventListener("click", function () {
+        loadJSONAndUpdateSVG("evening.json"); 
+    });
 });
