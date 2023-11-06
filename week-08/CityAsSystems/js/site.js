4afdc722196c62eccf4c8bb8ce9159f02d3d018a
Your Name <122640719+grassepark@users.noreply.github.com>
2023-11-06 13:42:06 -0500

    QuietUnion

--- week-08/CityAsSystems/js/site.js
+++ week-08/CityAsSystems/js/site.js
@@ -2,16 +2,6 @@ const courtyard = document.querySelector('#courtyard');
 const popup1 = document.querySelector('#popup1');
 const ampitheatre = document.querySelector('#ampitheatre');
 const popup2 = document.querySelector('#popup2');
-const e14st_roadside = document.querySelector('#e14st_roadside')
-const popup3 = document.querySelector('#popup3');
-const the_steps_behind_the_chesslords = document.querySelector('#the_steps_behind_the_chesslords')
-const popup4 = document.querySelector('#popup4');
-const ralings_at_the_george_washington_statue = document.querySelector('#ralings_at_the_george_washington_statue')
-const popup5 = document.querySelector('#popup5');
-const the_long_wooden_benches_in_the_park = document.querySelector('#the_long_wooden_benches_in_the_park')
-const popup6 = document.querySelector('#popup6');
-const seating_area_near_the_lincoln_statue_and_the_playground_for_children = document.querySelector('#seating_area_near_the_lincoln_statue_and_the_playground_for_children')
-const popup7 = document.querySelector('#popup7');
 
 
 courtyard.addEventListener('mouseover', () => {
@@ -31,53 +21,6 @@ ampitheatre.addEventListener('mouseover', () => {
 ampitheatre.addEventListener('mouseout', () => {
     popup2.style.opacity = '0';
 });
-
-
-e14st_roadside.addEventListener('mouseover', () => {
-    popup3.style.opacity = '1';
-});
-
-e14st_roadside.addEventListener('mouseout', () => {
-    popup3.style.opacity = '0';
-});
-
-
-the_steps_behind_the_chesslords.addEventListener('mouseover', () => {
-    popup4.style.opacity = '1';
-});
-
-the_steps_behind_the_chesslords.addEventListener('mouseout', () => {
-    popup4.style.opacity = '0';
-});
-
-
-ralings_at_the_george_washington_statue.addEventListener('mouseover', () => {
-    popup5.style.opacity = '1';
-});
-
-ralings_at_the_george_washington_statue.addEventListener('mouseout', () => {
-    popup5.style.opacity = '0';
-});
-
-
-the_long_wooden_benches_in_the_park.addEventListener('mouseover', () => {
-    popup6.style.opacity = '1';
-});
-
-the_long_wooden_benches_in_the_park.addEventListener('mouseout', () => {
-    popup6.style.opacity = '0';
-});
-
-
-seating_area_near_the_lincoln_statue_and_the_playground_for_children.addEventListener('mouseover', () => {
-    popup7.style.opacity = '1';
-});
-
-seating_area_near_the_lincoln_statue_and_the_playground_for_children.addEventListener('mouseout', () => {
-    popup7.style.opacity = '0';
-});
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
     function loadJSONAndUpdateSVG(jsonURL) {
